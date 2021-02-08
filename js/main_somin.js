d0c6ef60a383d9d7f7147c3c3723bdf2d354cfd7
somin <qkrthals0524@naver.com>
2021-02-08 22:17:09 +0900

    활성 씬 결정,반영

--- js/main_somin.js
+++ js/main_somin.js
@@ -1,7 +1,8 @@
 (()=>{
 
     let yOffset =0;
-
+    let prevScrollHeight=0; //현재 스크롤 위치
+    let currentScene=0; //현재 활성화된(눈 앞에보고있는) 씬
     const sceneInfo =[
         {   
             //0
@@ -47,8 +48,36 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum*window.innerHeight;
             sceneInfo[i].objs.container.style.height =`${sceneInfo[i].scrollHeight}px`
         }
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for(let i =0; i<sceneInfo.length;i++){
+            totalScrollHeight+=sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >=yOffset){
+                currentScene=i;
+                break
+            }            
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
     }
     function scrollLoop(){
+        prevScrollHeight=0;
+        for(let i=0; i<currentScene.length; i++){
+            prevScrollHeight+=sceneInfo[i].scrollHeight;
+
+        }
+
+        if(yOffset>prevScrollHeight+ sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
+
+        }
+        if(yOffset <prevScrollHeight){
+            if (currentScene===0) return; //브라우저 바운스 효과로 인해 마이너스 되는 것을 방지
+            currentScene--;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
+
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
         
     }
     window.addEventListener('resize',setLayout);
@@ -56,6 +85,7 @@
         yOffset = window.pageYOffset;
         scrollLoop();
     })
-    setLayout();
+    window.addEventListener('load',setLayout);
+    window.addEventListener('resize',setLayout);
 
 })();
